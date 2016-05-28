fa12161e6aad64b795457d1750b89907ad39b494
Gerardo A <zdoggie@gmail.com>
2016-05-27 21:25:16 -0400

    reformatted; propTypes

--- app/Card.js
+++ app/Card.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import CheckList from './CheckList';
 import marked from 'marked';
 
 class Card extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             showDetails: false
         };
     }
-    
-    toggleDetails(){
-        this.setState({showDetails: !this.state.showDetails});
+
+    toggleDetails() {
+        this.setState({ showDetails: !this.state.showDetails });
     }
-    
+
     render() {
         let cardDetails;
-        if(this.state.showDetails){
+        if (this.state.showDetails) {
             cardDetails = (
                 <div className="card__details">
                     {/*{marked(this.props.description)}*/}
-                    <span dangerouslySetInnerHTML={{__html:marked(this.props.description)}} />
+                    <span dangerouslySetInnerHTML={{ __html: marked(this.props.description) }} />
                     <CheckList cardId={this.props.id} tasks={this.props.tasks} />
                 </div>
             );
         };
-        
+
         let sideColor = {
             position: 'absolute',
             zIndex: -1,
@@ -35,16 +35,25 @@ class Card extends Component {
             width: 7,
             backgroundColor: this.props.color
         };
-        
+
         return (
             <div className="card">
                 <div style={sideColor}/>
-                <div 
-                    className={this.state.showDetails? "card__title card__title--is-open" : "card__title"} 
-                    onClick={this.toggleDetails.bind(this)}>{this.props.title}</div>
+                <div
+                    className={this.state.showDetails ? "card__title card__title--is-open" : "card__title"}
+                    onClick={this.toggleDetails.bind(this) }>{this.props.title}</div>
                 {cardDetails}
             </div>
         );
     }
-}
+};
+
+Card.propTypes = {
+    id: PropTypes.number,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    color: PropTypes.string,
+    tasks: PropTypes.arrayOf(PropTypes.object)
+};
+
 export default Card;
