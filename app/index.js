445e5844fba90a03a09ca9226fcd3de1ebee95e8
Gerardo Arevalo <gerardo@atctech.co>
2016-07-18 11:50:29 -0400

    added optional packages, updated react

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import Hello from './Hello';
 
-render(
+ReactDOM.render(
   <Hello />,
   document.getElementById('root')
 );
--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import KanbanBoard from './KanbanBoard';
 
-render(
+ReactDOM.render(
   <KanbanBoard />,
   document.getElementById('root')
 );
