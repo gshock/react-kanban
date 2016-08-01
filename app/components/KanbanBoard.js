bf4777cbfd48af08fb35a1c092462674c959da96
Gerardo Arevalo <gerardo@atctech.co>
2016-08-01 12:25:58 -0400

    Updated KanbanBoard

--- app/components/KanbanBoard.js
+++ app/components/KanbanBoard.js
@@ -7,39 +7,26 @@ import { Link } from 'react-router';
 
 class KanbanBoard extends Component {
   render() {
-    let cardModal = this.props.children && React.cloneElement(this.props.children, {
-      cards: this.props.cards,
-      cardCallbacks: this.props.cardCallbacks
-    });
-
     return (
       <div className="app">
         <Link to='/new' className="float-button">+</Link>
-        
-        <List id='todo'
-          title="To Do"
-          cards={this.props.cards.filter((card) => card.status === "todo") }
-          cardCallbacks={this.props.cardCallbacks}
-          taskCallbacks={this.props.taskCallbacks} />
-        <List id='in-progress'
-          title="In Progress"
-          cards={this.props.cards.filter((card) => card.status === "in-progress") }
-          cardCallbacks={this.props.cardCallbacks}
-          taskCallbacks={this.props.taskCallbacks} />
-        <List id='done'
-          title='Done'
-          cards={this.props.cards.filter((card) => card.status === "done") }
-          cardCallbacks={this.props.cardCallbacks}
-          taskCallbacks={this.props.taskCallbacks} />
-        {cardModal}
+        <List id='todo' title="To Do" cards={
+          this.props.cards.filter((card) => card.status === "todo")
+        } />
+        <List id='in-progress' title="In Progress" cards={
+          this.props.cards.filter((card) => card.status == "in-progress")
+        } />
+        <List id='done' title='Done' cards={
+          this.props.cards.filter((card) => card.status == "done")
+        } />
+        {this.props.children}
       </div>
-    );
+    )
   }
-};
+}
+
 KanbanBoard.propTypes = {
-  cards: PropTypes.arrayOf(PropTypes.object),
-  cardCallbacks: PropTypes.object,
-  taskCallbacks: PropTypes.object
-};
+  cards: PropTypes.arrayOf(PropTypes.object)
+}
 
 export default DragDropContext(HTML5Backend)(KanbanBoard);
