b3cd7f50a11a944633dca0a48282f6893fc3d693
Gerardo Arevalo <gerardo@atctech.co>
2016-08-01 12:33:45 -0400

    Updated Card removing any reference to CardCallbacks and TaskCallbacks; change any calls to those props to action creator calls

--- app/components/Card.js
+++ app/components/Card.js
@@ -5,7 +5,7 @@ import { DragSource, DropTarget } from 'react-dnd';
 import constants from '../constants';
 import CheckList from './CheckList';
 import { Link } from 'react-router';
-
+import CardActionCreators from '../actions/CardActionCreators';
 
 let titlePropType = (props, propName, componentName) => {
   if (props[propName]) {
@@ -26,14 +26,16 @@ const cardDragSpec = {
     };
   },
   endDrag(props) {
-    props.cardCallbacks.persistCardDrag(props.id, props.status);
+    CardActionCreators.persistCardDrag(props);
   }
 }
 
 const cardDropSpec = {
   hover(props, monitor) {
     const draggedId = monitor.getItem().id;
-    props.cardCallbacks.updatePosition(draggedId, props.id);
+    if (props.id !== draggedId) {
+      CardActionCreators.updateCardPosition(draggedId, props.id);
+    }
   }
 }
 
@@ -61,7 +63,7 @@ class Card extends Component {
   }
 
   toggleDetails() {
-    this.setState({showDetails: !this.state.showDetails});
+    this.setState({ showDetails: !this.state.showDetails });
   }
 
   render() {
@@ -71,10 +73,10 @@ class Card extends Component {
     if (this.state.showDetails) {
       cardDetails = (
         <div className="card__details">
-          <span dangerouslySetInnerHTML={{__html:marked(this.props.description)}} />
-            <CheckList cardId={this.props.id}
-                       tasks={this.props.tasks}
-                       taskCallbacks={this.props.taskCallbacks} />
+          <span dangerouslySetInnerHTML={{ __html: marked(this.props.description) }} />
+          <CheckList cardId={this.props.id}
+            tasks={this.props.tasks}
+            taskCallbacks={this.props.taskCallbacks} />
         </div>
       );
     }
@@ -93,30 +95,30 @@ class Card extends Component {
       <div className="card">
         <div style={sideColor} />
         <div className="card__edit">
-          <Link to={'/edit/'+this.props.id}>✎</Link>
+          <Link to={'/edit/' + this.props.id}>✎</Link>
         </div>
         <div className={
-            this.state.showDetails? "card__title card__title--is-open" : "card__title"
-          } onClick={this.toggleDetails.bind(this)}>
+          this.state.showDetails ? "card__title card__title--is-open" : "card__title"
+        } onClick={this.toggleDetails.bind(this) }>
           {this.props.title}
         </div>
         <ReactCSSTransitionGroup transitionName="toggle"
-                                 transitionEnterTimeout={250}
-                                 transitionLeaveTimeout={250}>
+          transitionEnterTimeout={250}
+          transitionLeaveTimeout={250}>
           {cardDetails}
         </ReactCSSTransitionGroup>
       </div>
     ));
   }
 }
+
 Card.propTypes = {
   id: PropTypes.number,
   title: titlePropType,
   description: PropTypes.string,
   color: PropTypes.string,
-  tasks: PropTypes.arrayOf(PropTypes.object),
-  taskCallbacks: PropTypes.object,
-  cardCallbacks: PropTypes.object,
+  tasks: PropTypes.array,
+  status: PropTypes.string,
   connectDragSource: PropTypes.func.isRequired,
   connectDropTarget: PropTypes.func.isRequired
 };
