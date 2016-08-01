e03c0e560e953c3d2fd749d14bb2d979dbe64b33
Gerardo Arevalo <gerardo@atctech.co>
2016-08-01 12:36:18 -0400

    update CheckList get rid of any TaskCallback calls and insert TaskActionCreator calls

--- app/components/CheckList.js
+++ app/components/CheckList.js
@@ -1,9 +1,11 @@
 import React, { Component, PropTypes } from 'react';
+import TaskActionCreators from '../actions/TaskActionCreators';
 
 class CheckList extends Component {
-  checkInputKeyPress(evt){
-    if(evt.key === 'Enter'){
-      this.props.taskCallbacks.add(this.props.cardId, evt.target.value);
+  checkInputKeyPress(evt) {
+    if (evt.key === 'Enter') {
+      let newTask = { id: Date.now(), name: evt.target.value, done: false };
+      TaskActionCreators.addTask(this.props.cardId, newTask);
       evt.target.value = '';
     }
   }
@@ -12,12 +14,16 @@ class CheckList extends Component {
     let tasks = this.props.tasks.map((task, taskIndex) => (
       <li key={task.id} className="checklist__task">
         <input type="checkbox"
-               checked={task.done}
-               onChange={ this.props.taskCallbacks.toggle.bind(null, this.props.cardId, task.id, taskIndex) } />
+          checked={task.done}
+          onChange={
+            TaskActionCreators.toggleTask.bind(null, this.props.cardId, task, taskIndex)
+          } />
         {task.name}{' '}
         <a href="#"
-           className="checklist__task--remove"
-           onClick={ this.props.taskCallbacks.delete.bind(null, this.props.cardId, task.id, taskIndex) } />
+          className="checklist__task--remove"
+          onClick={
+            TaskActionCreators.deleteTask.bind(null, this.props.cardId, task, taskIndex)
+          } />
       </li>
     ));
 
@@ -27,14 +33,16 @@ class CheckList extends Component {
         <input type="text"
           className="checklist--add-task"
           placeholder="Type then hit Enter to add a task"
-          onKeyPress={this.checkInputKeyPress.bind(this)} />
+          onKeyPress={this.checkInputKeyPress.bind(this) } />
       </div>
     );
   }
 }
+
 CheckList.propTypes = {
   cardId: PropTypes.number,
   tasks: PropTypes.arrayOf(PropTypes.object),
   taskCallbacks: PropTypes.object
 };
+
 export default CheckList;
