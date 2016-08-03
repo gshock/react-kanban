c243496481d8b0c76b085285decb676477018b78
Gerardo Arevalo <gerardo@atctech.co>
2016-08-03 11:23:11 -0400

    define constants and actions for create draft, update draft create store

--- app/constants.js
+++ app/constants.js
@@ -4,8 +4,11 @@ export default {
   FETCH_CARDS_SUCCESS: 'fetch cards success',
   FETCH_CARDS_ERROR: 'fetch cards error',
 
-  TOGGLE_CARD_DETAILS: 'toggle card details', 
-  
+  TOGGLE_CARD_DETAILS: 'toggle card details',
+
+  CREATE_DRAFT: 'create draft',
+  UPDATE_DRAFT: 'update draft',
+
   CREATE_CARD: 'create card',
   CREATE_CARD_SUCCESS: 'create card success',
   CREATE_CARD_ERROR: 'create card error',
@@ -15,19 +18,19 @@ export default {
   UPDATE_CARD_ERROR: 'update card error',
   UPDATE_CARD_STATUS: 'update card status',
   UPDATE_CARD_POSITION: 'update card position',
-  
+
   PERSIST_CARD_DRAG: 'persist card drag',
   PERSIST_CARD_DRAG_SUCCESS: 'persist card drag success',
   PERSIST_CARD_DRAG_ERROR: 'persist card drag error',
-  
+
   CREATE_TASK: 'create task',
   CREATE_TASK_SUCCESS: 'create task success',
   CREATE_TASK_ERROR: 'create task error',
-  
+
   DELETE_TASK: 'delete task',
   DELETE_TASK_SUCCESS: 'delete task success',
   DELETE_TASK_ERROR: 'delete task error',
-  
+
   TOGGLE_TASK: 'toggle task',
   TOGGLE_TASK_SUCCESS: 'toggle task success',
   TOGGLE_TASK_ERROR: 'toggle task error'
